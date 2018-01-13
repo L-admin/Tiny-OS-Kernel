4492a145a1743a850803719b3e1006ce44e765df
lipan <562150688@qq.com>
2018-01-14 02:05:32 +0800

    interrupt a: add main.c

---
+++ kernel/main.c
@@ -0,0 +1,10 @@
+#include "../lib/kernel/print.h"
+#include "init.h"
+
+int main()
+{
+    put_str("I am kernel\n");
+    init_all();
+    asm volatile("sti");	     // 为演示中断处理,在此临时开中断
+    while(1);
+}
