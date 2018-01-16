a25a46c504efe0cf25c86aa1986b3c02bdd5fdc2
lipan <562150688@qq.com>
2018-01-17 03:24:10 +0800

    add string: add string.h

--- kernel/main.c
+++ kernel/main.c
@@ -6,8 +6,8 @@ int main(void)
 {
     put_str("I am kernel\n");
     init_all();
-    //asm volatile("sti");	     // 打开中断 sti 指令，已将除时钟之外的所有设备中断都屏蔽了
-    ASSERT(1 == 2);
+    asm volatile("sti");	     // 打开中断 sti 指令，已将除时钟之外的所有设备中断都屏蔽了
+    //ASSERT(1 == 2);
     while(1)
         ;
 }
