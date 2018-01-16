c27242558db1723cac28b0aa4aef500e1486058c
lipan <562150688@qq.com>
2018-01-17 03:42:14 +0800

    no

---
+++ kernel/main.c
@@ -0,0 +1,13 @@
+#include "../lib/kernel/print.h"
+#include "init.h"
+#include "debug.h"
+
+int main(void)
+{
+    put_str("I am kernel\n");
+    init_all();
+    //asm volatile("sti");	     // 打开中断 sti 指令，已将除时钟之外的所有设备中断都屏蔽了
+    ASSERT(1 == 2);
+    while(1)
+        ;
+}
