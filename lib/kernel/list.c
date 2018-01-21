bc813d8d942f4e3dc1de325bba2e4d07426f9351
lipan <562150688@qq.com>
2018-01-21 21:18:56 +0800

    add list

---
+++ lib/kernel/list.c
@@ -0,0 +1,102 @@
+#include "list.h"
+
+
+/* 初始化双向链表 */
+void list_init(struct list* list)
+{
+    list->head.prev = NULL;
+    list->head.next = &(list->tail);
+    list->tail.prev = &(list->head);
+    list->tail.next = NULL;
+}
+
+/* 判断链表是否为空,空时返回true,否则返回false */
+bool list_empty(struct list* plist)
+{
+    return plist->head.next == &(plist->tail);
+}
+
+/* 返回链表长度 */
+uint32_t list_len(struct list* plist)
+{
+    struct list_elem* elem = plist->head.next;
+    uint32_t len = 0;
+
+    while(elem != &(plist->tail))
+    {
+        len++;
+        elem = elem->next;
+    }
+
+    return len;
+}
+
+
+/* 把链表elem插入在元素before之前 */
+void list_insert_before(struct list_elem* before, struct list_elem* elem)
+{
+    // 实现原子操作，关掉中断
+    enum intr_status old_status = intr_disable();
+
+    before->prev->next = elem;
+
+    elem->prev = before->prev;
+    elem->next = before;
+
+    before->prev = elem;
+
+    intr_set_status(old_status);
+}
+
+/* 添加元素到列表队首,类似push_front操作 */
+void list_push(struct list* plist, struct list_elem* elem)
+{
+    list_insert_before(plist->head.next, elem);
+}
+
+/* 追加元素到链表队尾,类似队列的先进先出操作 */
+void list_append(struct list* plist, struct list_elem* elem)
+{
+    list_insert_before(plist->tail, elem);
+}
+
+/* 使元素pelem脱离链表 */
+void list_remove(struct list_elem* pelem)
+{
+    // 实现原子操作，关掉中断
+    enum intr_status old_status = intr_disable();
+
+    pelem->prev->next = pelem->next;
+    pelem->next->prev = pelem->prev;
+
+    intr_set_status(old_status);
+}
+
+
+/* 将链表第一个元素弹出并返回 */
+struct list_elem* list_pop(struct list* plist)
+{
+    struct list_elem* elem = plist->head.next;
+    list_remove(elem);
+    return elem;
+}
+
+/* 从链表中查找元素obj_elem,成功时返回true,失败时返回false */
+bool elem_find(struct list* plist, struct list_elem* obj_elem)
+{
+    struct list_elem* elem = plist->head.next;
+
+    if (list_empty(plist))
+        return NULL;
+
+    while(elem != &(plist->tail))
+    {
+        if (func(elem, arg))
+            return elem;
+        elem = elem->next;
+    }
+
+    return NULL;
+}
+
+
---
+++ lib/kernel/list.h
@@ -0,0 +1,42 @@
+#ifndef __LIB_KERBEL_LIST_H
+#define __LIB_KERBEL_LIST_H
+
+#include "../../kernel/interrupt.h"
+
+#define offset(struct_type,member) (int)(&((struct_type*)0)->member)
+#define elem2entry(struct_type, struct_member_name, elem_ptr) \
+     (struct_type*)((int)elem_ptr - offset(struct_type, struct_member_name))
+
+
+struct list_elem
+{
+    struct list_elem* prev;
+    struct list_elem* next;
+};
+
+
+/* 实现队列 */
+struct list
+{
+    struct list_elem head;  // head 队首，固定不变.第一个元素是head.next
+    struct list_elem tail;  // tail 队尾，固定不变.
+};
+
+
+typedef bool (function)(struct list_elem*, int arg);
+
+
+
+void list_init(struct list* list);
+bool list_empty(struct list* plist);
+uint32_t list_len(struct list* plist);
+void list_insert_before(struct list_elem* before, struct list_elem* elem);
+void list_push(struct list* plist, struct list_elem* elem);
+void list_append(struct list* plist, struct list_elem* elem);
+void list_remove(struct list_elem* pelem);
+struct list_elem* list_pop(struct list* plist);
+bool elem_find(struct list* plist, struct list_elem* obj_elem);
+
+
+
+#endif // LIST_H
